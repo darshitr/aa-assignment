a9b54a4e1c9e0fffdb983cddd26c556e99ff3d19
darshitr <69704738+darshitr@users.noreply.github.com>
2023-04-18 20:57:33 +0530

    Create 3-mysql.sql

---
+++ 3-mysql.sql
@@ -0,0 +1,5 @@
+select ncbi_id,species from Rfam.taxonomy where species like '%Panthera tigris%'
+
+
+/* 1) we can find 8 types of tigers in taxonomy table AND ncbi_id of sumatran tiger is 9695  */
+/* 2) */
